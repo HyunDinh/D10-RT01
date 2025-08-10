95273a668ede5a3a62f7ba03dac5ba9479448f8d
hungt <hungthinh16072005@gmail.com>
2025-08-10 12:27:33 +0700

    Update Vite configuration to use localhost for API and WebSocket proxies

--- webpage/vite.config.js
+++ webpage/vite.config.js
@@ -8,17 +8,16 @@ export default defineConfig({
     global: 'window', // Polyfill để ánh xạ global thành window
   },
   server: {
-    port: 3000,
     strictPort: true,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://54.206.70.171:8080/',
+        target: 'http://localhost:8080/',
         changeOrigin: true,
         secure: false,
       },
       '/ws': {
-        target: 'http://54.206.70.171:8080/',
+        target: 'http://localhost:8080/',
         ws: true,
         changeOrigin: true,
         secure: false
